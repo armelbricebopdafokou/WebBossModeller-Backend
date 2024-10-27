c3cb455f6236eec13aa8905865bb628521d9dad4
armelbricebopdafokou <148085269+armelbricebopdafokou@users.noreply.github.com>
2024-10-27 22:19:38 +0000

    Update main to output generated at ce5a88f

---
+++ Models/DbSchema.cs
@@ -0,0 +1,24 @@
+﻿namespace WebBossModellerSqlGenerator.Models
+{
+    public record DbSchema(string Name) :ISql
+    {
+        public List<DbTable> Tables { get; init; }
+        public string ToSqlForMSSSQL()
+        {
+            return $"\n CREATE SCHEMA [{Name}]; \n GO";
+        }
+
+        public string ToSqlForMySQL()
+        {
+            return $"CREATE SCHEMA {Name}";
+        }
+
+        public string ToSqlForPostgresSQL(bool isCaseSensitive)
+        {
+            if (isCaseSensitive) 
+                return $"CREATE SCHEMA \"{Name}\"";
+            else
+                return $"CREATE SCHEMA {Name}";
+        }
+    }
+}
---
+++ Models/DbTable.cs
@@ -0,0 +1,139 @@
+﻿namespace WebBossModellerSqlGenerator.Models
+{
+    public record DbTable: ISql
+    {
+        public string Name { get; init; }
+        public bool IsWeak { get; init; }
+        public List<DbColumn> Columns { get; init; }
+
+        public DbSchema Schema { get; init; }
+
+        public string ToSqlForMSSSQL()
+        {
+            var sql = $"CREATE {Name} ({Environment.NewLine}";
+            foreach (var col in Columns)
+            {
+                sql += col.ToSqlForMSSSQL() +"\n" ;
+            }
+            sql += ")";
+            return sql ;
+        }
+
+        public string ToSqlForMySQL()
+        {
+            var sql = $"CREATE {Name} ({Environment.NewLine}";
+            foreach (var col in Columns)
+            {
+                sql += col.ToSqlForMSSSQL() + "\n";
+            }
+            sql += ")";
+            return sql;
+        }
+
+        public string ToSqlForPostgresSQL(bool isCaseSensitive)
+        {
+            string sql=string.Empty ;
+            if(isCaseSensitive)
+                sql = $"CREATE  \"{Schema.Name}\".\"{Name}\" ({Environment.NewLine}";
+            else
+                sql = $"CREATE {Schema.Name}.{Name} ({Environment.NewLine}";
+
+
+            foreach (var col in Columns)
+            {
+                sql += col.ToSqlForMSSSQL() + "\n";
+            }
+            sql += ")";
+            return sql;
+        }
+
+        public string AddContrainstsPostgres(bool isSensitive=false)
+        {
+            if (Columns != null || Columns.Where(c => c.IsPrimaryKey).Count() > 1)
+                throw new Exception("No columns or too much primary key");
+
+            if(Columns != null && Columns.Where(c=> c.IsPrimaryKey && c.IsForeignKey).Count() > 0)
+                throw new Exception("A key cannot be a primary and a foreign key");
+
+            string sql = string.Empty ;
+            var col = GetPrimaryKey();
+            if (isSensitive == true)
+            {
+                sql = $"ALTER TABLE \"{Name}\"";
+                if (col != null)
+                    sql += $" ADD CONSTRAINT {this.Name.Substring(0, 3)}_pk  PRIMARY KEY ( \"{col.Name}\" ); \n";
+            }
+            else
+            {
+                sql = $"ALTER TABLE {Name}";
+                if (col != null)
+                    sql += "ADD CONSTRAINT " + this.Name.Substring(0, 3) + "_pk  PRIMARY KEY (" + col.Name + "); \n";
+            }
+                
+            foreach(var c in  Columns.Where(co => co.IsForeignKey))
+            {
+                if(isSensitive == true)
+                {
+                    sql += $" ALTER TABLE ADD CONSTRAINT {this.Name.Substring(0, 3)}_{c.ReferenceTable.Name.Substring(0, 3)}_fk FOREIGN KEY (\"{c.Name}\") REFERENCES \"{c.ReferenceTable.Name}\" (\"{c.ReferenceTable.GetPrimaryKey()}\"); \n ";
+                }
+                else
+                {
+                    sql += $" ALTER TABLE ADD CONSTRAINT {this.Name.Substring(0, 3)}_{c.ReferenceTable.Name.Substring(0, 3)}_fk FOREIGN KEY ({c.Name}) REFERENCES {c.ReferenceTable.Name} ({c.ReferenceTable.GetPrimaryKey()}); \n";
+                }
+            }
+            return sql;
+        }
+
+        public DbColumn? GetPrimaryKey()
+        {
+            return Columns.Where(col => col.IsPrimaryKey).SingleOrDefault();
+        }
+
+        public string AddContrainstsMSSQL()
+        {
+            if (Columns != null || Columns.Where(c => c.IsPrimaryKey).Count() > 1)
+                throw new Exception("No columns or too much primary key");
+
+            if (Columns != null && Columns.Where(c => c.IsPrimaryKey && c.IsForeignKey).Count() > 0)
+                throw new Exception("A key cannot be a primary and a foreign key");
+
+            string sql = string.Empty;
+            var col = GetPrimaryKey();
+           
+            sql = $"ALTER TABLE [{Name}]";
+            if (col != null)
+                sql += "ADD CONSTRAINT " + this.Name.Substring(0, 3) + "_pk  PRIMARY KEY ([" + col.Name + "]); \n";
+           
+            foreach (var c in Columns.Where(co => co.IsForeignKey))
+            {
+                 sql += $"ALTER TABLE ADD CONSTRAINT {this.Name.Substring(0,3)}_{c.ReferenceTable.Name.Substring(0,3)}_fk  FOREIGN KEY ([{c.Name}]) REFERENCES [{c.ReferenceTable.Name}] ([{c.ReferenceTable.GetPrimaryKey()}]); \n";
+            }
+
+            return sql ;
+        }
+
+        public string AddContrainstsMySQL()
+        {
+            if (Columns != null || Columns.Where(c => c.IsPrimaryKey).Count() > 1)
+                throw new Exception("No columns or too much primary key");
+
+            if (Columns != null && Columns.Where(c => c.IsPrimaryKey && c.IsForeignKey).Count() > 0)
+                throw new Exception("A key cannot be a primary and a foreign key");
+
+            string sql = string.Empty;
+            var col = GetPrimaryKey();
+
+            sql = $"ALTER TABLE {Name}";
+            if (col != null)
+                sql += "ADD CONSTRAINT " + this.Name.Substring(0, 3) + "_pk  PRIMARY KEY (" + col.Name + "); \n";
+
+            foreach (var c in Columns.Where(co => co.IsForeignKey))
+            {
+                sql += $"ALTER TABLE ADD CONSTRAINT {this.Name.Substring(0, 3)}_{c.ReferenceTable.Name.Substring(0, 3)}_fk  FOREIGN KEY ({c.Name}) REFERENCES {c.ReferenceTable.Name} ({c.ReferenceTable.GetPrimaryKey()}); \n";
+            }
+
+            return sql;
+        }
+
+    }
+}
